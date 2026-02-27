96f7d41c6ef683b6660890fce621ea3c1b4c2278
Sara Mahmoud <saramahmooud3@gmail.com>
2026-02-27 14:42:33 +0200

    update

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,8 +1,13 @@
 import  JoinPortal  from './pages/authentication/JoinPortal';
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
-    <JoinPortal />
+     <Router>
+      <Routes>
+        <Route path="/" element={<JoinPortal />} />
+      </Routes>
+    </Router>
   );
 }
 
